b8d19a2c8d3aaa2455087a4e384f37b3da5567d7
Haron Kipkorir <haron@pixsel.net>
2025-09-22 20:54:30 +0300

    Added the base api service project and updated the readme accordingly.

---
+++ src/Chaplin.UrlShortener.Api/Program.cs
@@ -0,0 +1,23 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", () => "Hello World!")
+    .Produces(200, typeof(string));
+
+app.Run();
